b70b803634692c3fb5b8528ef3fc39d5f2f47fe9
guruamit <105117009+guruamit@users.noreply.github.com>
2022-05-10 18:51:29 +0530

    Delete New folder (2) directory

--- New folder (2)/check_divisible_by_2_and_5.py
+++
@@ -1,8 +0,0 @@
-# 3. Take input from the user and check whether that value is divisible by 2 and 5
-num = int (input("Enter the number you want to check : "))
-if num % 5==0 and  num % 2==0 :
-    print("The number you entered is ",num,".","It is divisible by 2 and 5.")
-else:
-    print("The number you entered is ",num,".","It is not divisible by both 2 and 5")
-
-print("Thank You")  
