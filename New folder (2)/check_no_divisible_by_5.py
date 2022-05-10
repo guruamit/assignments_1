44f27b69e1324d009673fbd748fc4dc2f8c40fac
guruamit <105117009+guruamit@users.noreply.github.com>
2022-05-10 18:29:48 +0530

    Add files via upload

---
+++ New folder (2)/check_no_divisible_by_5.py
@@ -0,0 +1,8 @@
+# 2 Take input from the user and check whether that value is divisible by 5
+num = int (input("Enter the number you want to check : "))
+if num % 5==0 :
+    print("The number you entered is ",num,".","It is divisible by 5.")
+else:
+    print("The number you entered is ",num,".","It is not divisible by 5")
+
+print("Thank You")        
